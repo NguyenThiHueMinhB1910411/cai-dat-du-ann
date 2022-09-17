044d760b118de50cfa81e0ca1ef82f784fa12200
Minh Nguyễn <minhb1910411@student.ctu.edu.vn>
2022-09-17 15:00:02 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,15 +1,47 @@
+// const express = require("express");
+// const contactsRouter = require("./app/routes/contact.route");
+// const cors = require("cors");
+
+// const app = express();
+
+// app.use(cors());
+// app.use("/api/contacts", contactsRouter);
+// app.use(express.json());
+
+// app.get("/",(req, res) => {
+//     res.json({message: " Welcome to contact book application."});
+// });
+
+// module.exports = app;
+
 const express = require("express");
+const ApiError = require("./app/api-error");
+
 const contactsRouter = require("./app/routes/contact.route");
+
 const cors = require("cors");
+const { application } = require("express");
 
 const app = express();
 
-app.use(cors());
+ //app.use(cors());
 app.use("/api/contacts", contactsRouter);
-app.use(express.json());
+
+//handle 404 response
+app.use((req, res, next)=>{
+    return next(new ApiError(404, "Resource not found"));
+});
+//define error
+app.use((err,req,res,next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
+
+application.use(express.json());
 
 app.get("/",(req, res) => {
-    res.json({message: " Welcome to contact book application."});
+    res.json({message:" Welcome to contact book application."});
 });
 
 module.exports = app;
